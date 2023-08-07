5599f01167468f615bb2c702b3cb6f2a087581cc
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-08-07 21:52:18 +0300

    react-projects-2

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Course from './Course'
+// import { useState } from 'react'
+
+function App() {
+
+  // const [value, setValue] = useState(0)
+
+  // const clickSetValue = () => {
+  //   setValue(value + 1)
+  // }
+
+  return (
+    <>
+      {/* <div>
+        <h1>Kerem Kenan Eren</h1>
+        <button onClick={clickSetValue}>Arttır</button>
+        <p>{value}</p>
+      </div> */}
+
+      <Course />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Course.jsx
@@ -0,0 +1,17 @@
+// import Angular_img from "./assets/angular-image.png"
+// import React_img from "./assets/react-image.png"
+// import Vue_img from "./assets/vue-image.png"
+// import NextJs_img from "./assets/next.js.png"
+
+
+function Course() {
+    return (
+        <>
+            <div>
+                <h1>Kerem Kenan Eren</h1>
+            </div>
+        </>
+    );
+}
+
+export default Course;
