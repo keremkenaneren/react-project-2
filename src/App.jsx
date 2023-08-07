103d4bd6c6b0c3494d09da8602522f4f6439d5d5
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-08-08 00:18:52 +0300

    finishProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import './App.css'
 import Course from './Course'
-// import { useState } from 'react'
+import {useState} from "react";
+
+
+function getRandomCourse(){
+    const courseArray = ["Angular","ReactJs","NextJs","VueJs","Java"]
+    return  courseArray[Math.floor(Math.random()*courseArray.length)]
+}
 
 function App() {
 
+    
   // const [value, setValue] = useState(0)
 
   // const clickSetValue = () => {
   //   setValue(value + 1)
   // }
 
-  return (
+    const [courses, setCourse] = useState([]);
+    
+    const handleClick = () => {
+        setCourse([...courses, getRandomCourse()])
+    }
+
+    const courseList =   courses.map((course, index) => {
+        return <Course key={index} courseName={course}/>;
+    });
+
+    return (
     <>
       {/* <div>
         <h1>Kerem Kenan Eren</h1>
         <button onClick={clickSetValue}>Arttır</button>
         <p>{value}</p>
       </div> */}
+        <button onClick={handleClick}>Add Course</button>
+       <div className="wrapper">
+           {courseList}
+       </div>
 
-      <Course />
 
     </>
   )
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,14 +1,24 @@
-// import Angular_img from "./assets/angular-image.png"
-// import React_img from "./assets/react-image.png"
-// import Vue_img from "./assets/vue-image.png"
-// import NextJs_img from "./assets/next.js.png"
+import Angular_img from "./assets/angular-image.png"
+import React_img from "./assets/react-image.png"
+import Vue_img from "./assets/vue-image.png"
+import NextJs_img from "./assets/next.js.png"
+import Java_img from "./assets/java.png"
 
+const courseMap = {
+    Angular:Angular_img,
+    ReactJs:React_img,
+    NextJS:NextJs_img,
+    VueJs:Vue_img,
+    Java:Java_img
+}
 
-function Course() {
+// eslint-disable-next-line react/prop-types
+function Course({courseName}) {
     return (
         <>
             <div>
-                <h1>Kerem Kenan Eren</h1>
+                {/*<h1>Kerem Kenan Eren <u>Courses</u></h1>*/}
+                <img src={courseMap[courseName]} alt=""/>
             </div>
         </>
     );
